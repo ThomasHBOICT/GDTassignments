53de5b1b44f1e95873dadf1ee2d93a7439ab6812
Thomas <393890@student.fontys.nl>
2020-03-27 11:50:13 +0100

    endless road and camera follow

---
+++ assignments/Assets/block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform nextPos;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Transform getNextPos()
+    {
+        return nextPos; 
+    }
+}
---
+++ assignments/Assets/factory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class factory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public block objectToSpawn;
+    public float spawnRate;
+
+    private Transform nextPosition;
+    void Start()
+    {
+        nextPosition = transform;
+        StartCoroutine(startSpawning());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator startSpawning()
+    {
+        while (true)
+        {
+            block spawnedPiece = Instantiate(objectToSpawn, nextPosition);
+            nextPosition = spawnedPiece.getNextPos();
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}
