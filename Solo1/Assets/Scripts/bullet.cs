0b987a5b12ad0d5adcedcb2388af4f3560996271
Thomas <393890@student.fontys.nl>
2020-04-23 17:28:21 +0200

    particle system on bullet hit added

--- Solo1/Assets/Scripts/bullet.cs
+++ Solo1/Assets/Scripts/bullet.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public GameObject particles;
     public Color[] colors;
+    private Transform tf;
+    
+   
     void Start()
     {
+        tf = GetComponent<Transform>();
         Color randomColor = colors[Random.Range(0, colors.Length)];
         gameObject.GetComponent<Renderer>().material.color = randomColor;
         Destroy(gameObject, 3f);
@@ -14,6 +19,7 @@ public class bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Instantiate(particles, tf.position, Quaternion.Euler(-90, 0 ,0));
         Destroy(gameObject);
     }
 }
---
+++ Solo1/Assets/onHitParticles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onHitParticles : MonoBehaviour
+{
+    public Gradient particleColorGradient;
+    private ParticleSystem ps;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        var main = ps.main;
+        main.startColor = particleColorGradient.Evaluate(Random.Range(0f, 1f));
+        Destroy(gameObject, 1);
+    }
+}
