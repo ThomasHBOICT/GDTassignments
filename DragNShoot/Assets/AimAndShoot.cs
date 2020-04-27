1e1d965b8e427fe095160518a316a8d63a13587d
Thomas <393890@student.fontys.nl>
2020-04-27 22:55:11 +0200

    linerenderer

--- DragNShoot/Assets/AimAndShoot.cs
+++ DragNShoot/Assets/AimAndShoot.cs
@@ -7,19 +7,23 @@ public class AimAndShoot : MonoBehaviour
     public float shootForce;
     Vector2 mouseWorldPointDown;
     Vector2 mouseWorldPointUp;
-    Vector2 targetToMouse;
     Vector2 mouseDownToUp;
     Rigidbody2D rb;
+    private bool canShoot = true;
+    private LineRenderer line;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Aiming();
+        shootState();
+        DrawLine();
     }
 
     void Aiming()
@@ -27,17 +31,32 @@ public class AimAndShoot : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             mouseWorldPointDown = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetToMouse = mouseWorldPointDown - rb.position;
-
-           
         }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (Input.GetKeyUp(KeyCode.Mouse0) && canShoot)
         {
             mouseWorldPointUp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mouseDownToUp = mouseWorldPointDown - mouseWorldPointUp;
             rb.AddForce(mouseDownToUp * shootForce, ForceMode2D.Impulse);
+            canShoot = false;
         }
-       
+    }
+
+    void shootState()
+    {
+        if (rb.velocity.magnitude < 0.1f)
+        {
+            canShoot = true;
+        }
+    }
+
+    void DrawLine()
+    {
+        while (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector2 linePointOne = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            line.SetPosition(0, linePointOne);
+            line.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
     }
 }
