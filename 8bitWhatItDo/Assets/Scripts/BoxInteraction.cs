6c84385d0d583c524cda63551592613d338c763b
Thomas <393890@student.fontys.nl>
2020-05-04 21:15:38 +0200

    init

---
+++ 8bitWhatItDo/Assets/Scripts/BoxInteraction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxInteraction : MonoBehaviour
+{
+    public PlayerInteraction playerInteraction;
+    public GameObject Item;
+    public GameObject Hat;
+    private bool inRange = false;
+
+    public enum Pickups { Hat, Item, Nothing };
+
+    public Pickups toDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pickup();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+
+    private void Pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inRange && toDrop == Pickups.Item)
+        {
+            playerInteraction.HandPickup(Item);
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && inRange && toDrop == Pickups.Hat)
+        {
+            playerInteraction.HeadPickup(Hat);
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && inRange && toDrop == Pickups.Nothing)
+        {
+        }
+    }
+}
---
+++ 8bitWhatItDo/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject Item;
+    public Transform hand;
+    public Transform head;
+
+    public void HandPickup(GameObject Item)
+    {
+        Instantiate(Item, hand.transform.position, gameObject.transform.rotation, hand.transform);
+    }
+
+    public void HeadPickup(GameObject Item)
+    {
+        Instantiate(Item, head.transform.position, gameObject.transform.rotation, head.transform);
+    }
+}
