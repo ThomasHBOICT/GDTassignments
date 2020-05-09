36618076078d0c9cdc76dffe3efa0ffe46c56a64
Thomas <393890@student.fontys.nl>
2020-05-09 15:14:18 +0200

    first gameplay, switch dimentions and movement

---
+++ WeeklyGameJam148/Assets/PortalInteraction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PortalInteraction : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public GameObject[] enemies;
+    public Color dimentionColor;
+    public Sprite coinSprite;
+    public Sprite enemySprite;
+
+    private bool isFlipped = false;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("portal entered.");
+            ChangeMapColor();
+            ChangeEnemies();
+        }
+    }
+
+    private void ChangeEnemies()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy.GetComponent<SpriteRenderer>().sprite == coinSprite)
+            {
+                enemy.GetComponent<SpriteRenderer>().sprite = enemySprite;
+                enemy.GetComponent<EnemyMovement>().enabled = true;
+            }
+            else
+            {
+                enemy.GetComponent<SpriteRenderer>().sprite = coinSprite;
+                enemy.GetComponent<EnemyMovement>().enabled = false;
+            }
+        }        
+    }
+
+    private void ChangeMapColor()
+    {
+        isFlipped = !isFlipped;
+        if (isFlipped)
+        {
+            tilemap.color = dimentionColor;
+        }
+        else
+        {
+            tilemap.color = new Color(0.75f, 1, 0.32f, 1);
+        }
+    }
+}
---
+++ WeeklyGameJam148/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float maxSpawnTimer;
+
+    private float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= maxSpawnTimer)
+        {
+
+            float Xrange = Random.Range(-8, 8);
+            float Zrange = Random.Range(-4.5f, 4.5f);
+
+            Vector3 spawnPos = new Vector3(Xrange, 1, Zrange);
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            spawnTimer = 0f;
+        }
+    }
+}
